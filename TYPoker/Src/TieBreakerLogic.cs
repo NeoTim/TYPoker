561f548eff7ad96ce928dfb279fda02565e9dedd
Tai An <taian@tais-mac-mini.lan>
2017-08-17 23:29:28 -0700

    Fix indent; use spaces

--- TYPoker/Src/TieBreakerLogic.cs
+++ TYPoker/Src/TieBreakerLogic.cs
@@ -12,19 +12,19 @@ namespace TYPoker.Src
         public const Int64 kTwoTo16Minus1 = 65535;
 
         // --------------- Straight Flush Tie Breaker ------------------
-		// If two players have straight flush, the cards must be in the same suit
+        // If two players have straight flush, the cards must be in the same suit
         // for 2 + 2 + 3 + 1 + 1 < 5 x 2
         //
         // Tie: straight flush cards are community cards
         // Utility Values are designed as below
-		// 10, J, Q, K, A --> 0xe // 14
-		// 9, 10, J, Q, K --> 0xd // 13
-		// ....
-		// 2, 3, 4, 5, 6 --> 0x6
-		// A, 2, 3, 4, 5 --> 0x5
+        // 10, J, Q, K, A --> 0xe // 14
+        // 9, 10, J, Q, K --> 0xd // 13
+        // ....
+        // 2, 3, 4, 5, 6 --> 0x6
+        // A, 2, 3, 4, 5 --> 0x5
         // (Value is the highest card in the straight)
         // AddVal = [0x5, 0xe]
-		public static Int64 CalStraightFlushTB(Int64 bits)
+        public static Int64 CalStraightFlushTB(Int64 bits)
         {
             if (bits == 0) return 0;
 
@@ -50,19 +50,19 @@ namespace TYPoker.Src
             return addVal;
         }
 
-		// --------------- Four of a kind Tie Breaker ------------------
-		// Scenario 1: AAAAJ, 6666A, bigger four of a kind wins
-		// Scenario 2: four of kind cards are community cards, check high card
-		//              if high card is still shared, tie
-		//              
-		// To generalize, use 2 hexadecimal digits for Four Of A Kind value
-		// 1st digit is the four of a kind card
-		// 2nd digit is the single card that has the highest value
-		// e.g. AAAAJ  --> 0xeb
-		//      6666A  --> 0x6e
+        // --------------- Four of a kind Tie Breaker ------------------
+        // Scenario 1: AAAAJ, 6666A, bigger four of a kind wins
+        // Scenario 2: four of kind cards are community cards, check high card
+        //              if high card is still shared, tie
+        //              
+        // To generalize, use 2 hexadecimal digits for Four Of A Kind value
+        // 1st digit is the four of a kind card
+        // 2nd digit is the single card that has the highest value
+        // e.g. AAAAJ  --> 0xeb
+        //      6666A  --> 0x6e
         //
-		// addVal = [0x23, 0xed] ~ [35, 237]
-		public static Int64 CalFourOfAKindTB(Int32 tb1, Int64 tb_bits)
+        // addVal = [0x23, 0xed] ~ [35, 237]
+        public static Int64 CalFourOfAKindTB(Int32 tb1, Int64 tb_bits)
         {
             if (tb1 == 0 && tb_bits == 0) return 0;
 
@@ -79,28 +79,28 @@ namespace TYPoker.Src
         }
 
 
-		// --------------- Fullhouse Tie Breaker ------------------
+        // --------------- Fullhouse Tie Breaker ------------------
         // 1st digit: three of a kind
         // 2nd digit: high pair
         //
         // addVal = [0x23, 0xed] ~ [35, 237]
         public static Int64 CalFullHouseTB(Int32 threeOfAkindValue, Int64 pairValue)
-		{
-			if (pairValue == 0) return 0;
-			Int64 addVal = 0x00;
+        {
+            if (pairValue == 0) return 0;
+            Int64 addVal = 0x00;
 
-			addVal += threeOfAkindValue << 4;
-			Console.WriteLine("tb1 = " + threeOfAkindValue + ", addVal = " + addVal);
+            addVal += threeOfAkindValue << 4;
+            Console.WriteLine("tb1 = " + threeOfAkindValue + ", addVal = " + addVal);
 
             addVal += pairValue;
             Console.WriteLine("pair = " + pairValue + ", addVal = " + addVal);
 
-			return addVal;
-		}
+            return addVal;
+        }
 
 
-		// --------------- Flush Tie Breaker ------------------
-		// A flush is any hand with five cards of the same suit. If two or more players hold a flush,
+        // --------------- Flush Tie Breaker ------------------
+        // A flush is any hand with five cards of the same suit. If two or more players hold a flush,
         // the flush with the highest card wins. If more than one player has the same strength high card,
         // then the strength of the second highest card held wins. This continues through the five highest
         // cards in the player's hands.
@@ -115,7 +115,7 @@ namespace TYPoker.Src
         // Only care about the highest 5 cards
         //
         // addVal = [94, 15616] < 0x4000
-		public static Int64 CalFlushTieBreaker(Int64 bits)
+        public static Int64 CalFlushTieBreaker(Int64 bits)
         {
             Int64 addVal = 0x00;
 
@@ -123,24 +123,23 @@ namespace TYPoker.Src
             while(BitOperations.CountBits((uint)bits) > 5)
             {
                 Int32 lowBit = (Int32)BitOperations.CalLowBit((Int32)bits) + 1;
-
                 bits = (bits >> lowBit) << lowBit;
             }
 
-			Util.BinaryPrint((Int32)bits, "High 5 flush cards: ");
+            Util.BinaryPrint((Int32)bits, "High 5 flush cards: ");
             addVal += bits;
-			Console.WriteLine("flush = " + bits + ", addVal = " + addVal);
+            Console.WriteLine("flush = " + bits + ", addVal = " + addVal);
 
             return addVal;
         }
 
-		// --------------- Straight Tie Breaker ------------------
-		// Pick highest straight
-		// addVal = [0x5, 0xe] 
-		public static Int64 CalStraightTB(Int64 bits)
-		{
-			if (bits == 0) return 0;
-			Int64 addVal = 0x00;
+        // --------------- Straight Tie Breaker ------------------
+        // Pick highest straight
+        // addVal = [0x5, 0xe] 
+        public static Int64 CalStraightTB(Int64 bits)
+        {
+            if (bits == 0) return 0;
+            Int64 addVal = 0x00;
 
             Util.BinaryPrint((Int32)bits, "Straight cards: ");
             uint highBit = BitOperations.CalHighBit((Int32)bits) + 1;
@@ -148,51 +147,50 @@ namespace TYPoker.Src
             Console.WriteLine("add Straight util val = " + highBit);
 
             addVal = highBit;
-			return addVal;
-		}
+            return addVal;
+        }
 
 
-		// --------------- Three of a kind Tie Breaker ------------------
-		// High threeOfAkind + 2 single cards
-		//           0x[2-e]0000 + 0xffff (int converted from straight bits)
-		//                         2^12 + 2^13 = 12288 (K,A)
-		//                         2^1  + 2^2  = 6     (2,3)
-		// addVal < 1048575 (0x100000)
-		// << 16 three of a kind value + high 2 bits
-		// (14 x 65536 + 16384 = 933,888 < 0x100000 = 1,048,576)
-		public static Int64 CalThreeOfAKindTB(Int32 tb1, Int64 bits)
-		{
+        // --------------- Three of a kind Tie Breaker ------------------
+        // High threeOfAkind + 2 single cards
+        //           0x[2-e]0000 + 0xffff (int converted from straight bits)
+        //                         2^12 + 2^13 = 12288 (K,A)
+        //                         2^1  + 2^2  = 6     (2,3)
+        // addVal < 1048575 (0x100000)
+        // << 16 three of a kind value + high 2 bits
+        // (14 x 65536 + 16384 = 933,888 < 0x100000 = 1,048,576)
+        public static Int64 CalThreeOfAKindTB(Int32 tb1, Int64 bits)
+        {
             Console.WriteLine("TB: three of a kind: " + tb1);
 
             //Util.BinaryPrint((int)bits, "single cards bit: ");
 
-			if (tb1 == 0 && bits == 0) return 0;
+            if (tb1 == 0 && bits == 0) return 0;
 
-			Int64 addVal = tb1 << 16; // make sure it's bigger than the single cards
+            Int64 addVal = tb1 << 16; // make sure it's bigger than the single cards
             Console.WriteLine("TB: three of a kind card addVal = " + addVal);
 
 
-			// Compare highest 2 cards
-			while (BitOperations.CountBits((uint)bits) > 2)
-			{
-				Int32 lowBit = (Int32)BitOperations.CalLowBit((Int32)bits) + 1;
+            // Compare highest 2 cards
+            while (BitOperations.CountBits((uint)bits) > 2)
+            {
+                Int32 lowBit = (Int32)BitOperations.CalLowBit((Int32)bits) + 1;
+                bits = (bits >> lowBit) << lowBit;
+            }
 
-				bits = (bits >> lowBit) << lowBit;
-			}
-
-			Util.BinaryPrint((Int32)bits, "High 2 single cards: ");
-			addVal += bits;
+            Util.BinaryPrint((Int32)bits, "High 2 single cards: ");
+            addVal += bits;
             Console.WriteLine("TB: Three of a kind, addVal = " + addVal + " (singles = " + bits + ")");
 
-			return addVal;
-		}
+            return addVal;
+        }
 
 
         // << 4 pair bits + high card value
-		public static Int64 CalTwoPairTB(Int32 tb1, Int64 bits)
-		{
-			if (tb1 == 0 && bits == 0) return 0;
-			Int64 addVal = 0x00;
+        public static Int64 CalTwoPairTB(Int32 tb1, Int64 bits)
+        {
+            if (tb1 == 0 && bits == 0) return 0;
+            Int64 addVal = 0x00;
 
             Util.BinaryPrint((Int32)bits, "TB: TwoPair bits: ");
 
@@ -200,54 +198,49 @@ namespace TYPoker.Src
             addVal += tb1;          // highest single card
 
             Console.WriteLine("TB: TwoPair " + addVal + " (high card = " + tb1 + ")");
-			return addVal;
-		}
+            return addVal;
+        }
 
         // << 16 pair value + high 3 bits
         public static Int64 CalOnePairTB(Int32 tb1, Int64 bits)
         {
-			if (tb1 == 0 && bits == 0) return 0;
-			Int64 addVal = 0x00;
+            if (tb1 == 0 && bits == 0) return 0;
+            Int64 addVal = 0x00;
 
             Console.WriteLine("TB: OnePair, has a pair of " + tb1);
             addVal += tb1 << 16; // the pair
 
-
-			// Compare highest 3 single cards
-			while (BitOperations.CountBits((uint)bits) > 3)
-			{
-				Int32 lowBit = (Int32)BitOperations.CalLowBit((Int32)bits) + 1;
-				bits = (bits >> lowBit) << lowBit;
-			}
+            // Compare highest 3 single cards
+            while (BitOperations.CountBits((uint)bits) > 3)
+            {
+                Int32 lowBit = (Int32)BitOperations.CalLowBit((Int32)bits) + 1;
+                bits = (bits >> lowBit) << lowBit;
+            }
 
             addVal += bits;      // the rest single bits, no need to select high 3
-
 
             Util.BinaryPrint((Int32)bits, "TB: OnePair Single card bits: ");
             Console.WriteLine("TB: OnePair " + addVal);
             return addVal;
         }
 
-		
-		public static Int64 CalHighCardTB(Int64 bits)
-		{
-			if (bits == 0) return 0;
-			Int64 addVal = 0x00;
+        public static Int64 CalHighCardTB(Int64 bits)
+        {
+            if (bits == 0) return 0;
+            Int64 addVal = 0x00;
 
-			// Compare highest 5 cards
-			while (BitOperations.CountBits((uint)bits) > 5)
-			{
-				Int32 lowBit = (Int32)BitOperations.CalLowBit((Int32)bits) + 1;
-
-				bits = (bits >> lowBit) << lowBit;
-			}
+            // Compare highest 5 cards
+            while (BitOperations.CountBits((uint)bits) > 5)
+            {
+                Int32 lowBit = (Int32)BitOperations.CalLowBit((Int32)bits) + 1;
+                bits = (bits >> lowBit) << lowBit;
+            }
 
             addVal += bits;
 
-			Util.BinaryPrint((Int32)bits, "TB: High Card bits: ");
-			Console.WriteLine("TB: High Card " + addVal);
-			return addVal;
-		}
-		
-	}
+            Util.BinaryPrint((Int32)bits, "TB: High Card bits: ");
+            Console.WriteLine("TB: High Card " + addVal);
+            return addVal;
+        }
+    }
 }
